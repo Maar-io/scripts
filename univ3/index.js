1e38eeb1eefbcfcd8585a6c77d53d249af1eeb2e
Mar.io <34627453+Maar-io@users.noreply.github.com>
2024-04-25 20:57:04 +0200

    get price

--- univ3/index.js
+++ univ3/index.js
@@ -1,38 +1,122 @@
 const { ethers } = require('ethers');
-const { Pool, Token, Route, priceToClosestTick } = require('@uniswap/v3-sdk');
-const { Token: TokenV2, ChainId, WETH } = require('@uniswap/sdk-core');
-const { NonfungiblePositionManager } = require('@uniswap/v3-periphery');
+const { Pool, Route, priceToClosestTick } = require('@uniswap/v3-sdk');
+const { Token } = require('@uniswap/sdk-core');
+// const { IUniswapV3Factory } = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json');
+
+// const { NonfungiblePositionManager } = require('@uniswap/v3-periphery');
+const FACTORY_ABI = require("./abi/factory.json");
+const POOL_ABI = require("./abi/pool.json");
+const ERC20_ABI = require("./abi/ERC20.json");
+
+require("dotenv").config();
 
 const ZKASTAR_MAINNET = 3776;
+
 // Swap usdc/astr on QS
 // https://astar-zkevm.explorer.startale.com/tx/0xc0ab00fc49ccc8e7778fa5b0527079304d9258f81480f6ec1a65763a1a1d29df
 
 // Swap astr/usdc on QS
 // https://astar-zkevm.explorer.startale.com/tx/0xe1f7c32a0e4a9be94a66377b2d92fd5c2c6ef8611c9d70b2bf0783f6d6fe3cad
 
-async function getPrice() {
-  const provider = new ethers.providers.JsonRpcProvider('https://rpc.startale.com/astar-zkevm');
+async function getPrice(factory, tokenInContractAddress, tokenOutContractAddress, commission) {
+  const chainId = ZKASTAR_MAINNET;
+  const provider = new ethers.providers.JsonRpcProvider('https://rpc.startale.com/astar-zkevm', ZKASTAR_MAINNET);
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  const ASTR = new TokenV2(ZKASTAR_MAINNET, '0xdf41220C7e322bFEF933D85D01821ad277f90172', 18); // ASTR
-  const USDC = new TokenV2(ZKASTAR_MAINNET, '0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035', 6); // USDC
+  // create token contracts and related objects
+  const contractIn = new ethers.Contract(tokenInContractAddress, ERC20_ABI, signer);
+  const contractOut = new ethers.Contract(tokenOutContractAddress, ERC20_ABI, signer);
 
-  const USDC_ASTR_POOL = '0x8BCbd2b0240C3e54D803E33Ba944Fa98267312fF'; // QS USDC/ASTR UniswapV3Pool
-  const ASTR_USDC_POOL = '0x4a3A7559F64f1260EB86dAa7c3928843eD2C29A7'; // QS ASTR/USDC UniswapV3Pool
+  const getTokenAndBalance = async function (contract) {
+    var [dec, symbol, name] = await Promise.all(
+      [
+        contract.decimals(),
+        contract.symbol(),
+        contract.name(),
+      ]);
 
-  const poolContract = new ethers.Contract(ASTR_USDC_POOL, NonfungiblePositionManager.abi, provider);
-  const poolData = await poolContract.slot0();
+    return [new Token(chainId, contract.address, dec, symbol, name)];
+  }
+
+  const [tokenIn] = await getTokenAndBalance(contractIn);
+  const [tokenOut] = await getTokenAndBalance(contractOut);
+
+  console.log(`   Input: ${tokenIn.symbol} (${tokenIn.name})`);
+  console.log(`   Output: ${tokenOut.symbol} (${tokenOut.name})`);
+  console.log("");
+
+  // Get the pair pool address
+  console.log("Loading pool information...");
+  const factoryContract = new ethers.Contract(factory, FACTORY_ABI, provider);
+  const poolAddress = await factoryContract.getPool(
+    tokenIn.address,
+    tokenOut.address,
+    commission);  // 3% = 3000
+
+  if (Number(poolAddress).toString() === "0") // there is no such pool for provided In-Out tokens.
+    throw `Error: No pool ${tokenIn.symbol}-${tokenOut.symbol}`;
+  else console.log(`${tokenIn.symbol}-${tokenOut.symbol} Pool address: ${poolAddress}, commission=${commission/10000}%`);
+
+  const poolContract = new ethers.Contract(poolAddress, POOL_ABI, provider);
+
+  const getPoolState = async function () {
+    const [liquidity, slot] = await Promise.all([poolContract.liquidity(), poolContract.slot0()]);
+
+    return {
+      liquidity: liquidity,
+      sqrtPriceX96: slot[0],
+      tick: slot[1],
+      observationIndex: slot[2],
+      observationCardinality: slot[3],
+      observationCardinalityNext: slot[4],
+      feeProtocol: slot[5],
+      unlocked: slot[6],
+    }
+  }
+
+  const getPoolImmutables = async function () {
+    const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] = await Promise.all([
+      poolContract.factory(),
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee(),
+      poolContract.tickSpacing(),
+      poolContract.maxLiquidityPerTick(),
+    ]);
+
+    return {
+      factory: factory,
+      token0: token0,
+      token1: token1,
+      fee: fee,
+      tickSpacing: tickSpacing,
+      maxLiquidityPerTick: maxLiquidityPerTick,
+    }
+  }
+  // loading immutable pool parameters and its current state (variable parameters)
+  const [immutables, state] = await Promise.all([getPoolImmutables(), getPoolState()]);
 
   const pool = new Pool(
-    ASTR,
-    USDC,
-    poolData.fee,
-    poolData.sqrtPriceX96.toString(),
-    poolData.liquidity.toString(),
-    poolData.tick
+    tokenIn,
+    tokenOut,
+    immutables.fee,
+    state.sqrtPriceX96.toString(),
+    state.liquidity.toString(),
+    state.tick
   );
 
-  const route = new Route([pool], ASTR, USDC);
-  console.log(`1 ${ASTR.symbol} = ${route.midPrice.toSignificant(6)} ${USDC.symbol}`);
+  // print token prices in the pool
+  console.log("Token prices in pool:");
+  console.log(`   1 ${pool.token0.symbol} = ${pool.token0Price.toSignificant()} ${pool.token1.symbol}`);
+  console.log(`   1 ${pool.token1.symbol} = ${pool.token1Price.toSignificant()} ${pool.token0.symbol}`);
+  console.log(`Pool liquidity: ${pool.liquidity}`);
+  console.log('');
 }
 
-getPrice().catch(console.error);
+const ASTR_ADDRESS = '0xdf41220C7e322bFEF933D85D01821ad277f90172'
+const USDC_ADDRESS = '0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035'
+const UNISWAP_FACTORY_ADDRESS = '0x56c2162254b0E4417288786eE402c2B41d4e181e'; // QuickSwap
+const COMMISSION = 100
+
+getPrice(UNISWAP_FACTORY_ADDRESS, ASTR_ADDRESS, USDC_ADDRESS, COMMISSION)
+  .catch(console.error);
